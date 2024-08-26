8fd9884a54dee32d305060eae2b78e18767b110d
MAUS <126576476+MAUS1079@users.noreply.github.com>
2024-08-26 08:27:59 +0200

    Changes ch. 4.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
-import BooksList from './BooksList.container';
+import BooksList from './BooksList';
+import withBooks from './withBooks';
+
+const BooksListWithBooks = withBooks(BooksList);
 
 function App() {
   return (
     <div>
       <h1>Books management</h1>
-      <BooksList />
+      <BooksListWithBooks />
     </div>
   );
 }
